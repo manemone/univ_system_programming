e4ba63ff56e7788d965b9cbef4acadc3d0492252
HAMADA Kazuki <hamada.kazuki.mum@gmail.com>
2013-11-21 03:25:28 +0900

    mutex を利用して銀行口座ライブラリを実装。単一スレッドでの動作を確認。

--- practice_05/bank_account/banking.c
+++ practice_05/bank_account/banking.c
@@ -1,13 +1,20 @@
 #include <pthread.h>
 #include "banking.h"
 
+// 口座残高
 static int balance = 0;
+
+// 残高の mutex
+pthread_mutex_t mutex_balance = PTHREAD_MUTEX_INITIALIZER;
 
 /**
  * 口座へ入金する。
  * 処理が成功すると 1 を返す。
  **/
 void deposit (int n) {
+  pthread_mutex_lock(&mutex_balance);
+  balance += n;
+  pthread_mutex_unlock(&mutex_balance);
 }
 
 /**
@@ -16,11 +23,31 @@ void deposit (int n) {
  * 残高不足により処理が行えなければ 0 を返す。
  **/
 int withdraw (int n) {
+  int ret;
+
+  pthread_mutex_lock(&mutex_balance);
+  if (balance-n < 0) {
+    ret = 0;
+  }
+  else {
+    balance -= n;
+    ret = 1;
+  }
+  pthread_mutex_unlock(&mutex_balance);
+
+  return ret;
 }
 
 /**
  * 残高を返す。
  **/
 int get_balance (void) {
+  int ret;
+
+  pthread_mutex_lock(&mutex_balance);
+  ret = balance; 
+  pthread_mutex_unlock(&mutex_balance);
+
+  return ret;
 }
 
--- practice_05/bank_account/main.c
+++ practice_05/bank_account/main.c
@@ -2,5 +2,26 @@
 #include "banking.h"
 
 int main (void) {
+  int value;
+  int succeeded;
+  int i;
+
+  for (i = 0, value = 100; i < 10; i++) {
+    deposit(value);
+    printf("depositted: %d\n", value);
+    printf("balance: %d\n", get_balance());
+  }
+
+  for (i = 0, value = 200, succeeded = 0; i < 10; i++) {
+    succeeded = withdraw(value);
+    if (succeeded) {
+      printf("withdrawed: %d\n", value);
+    }
+    else {
+      printf("withdrawing failed. insufficient fund.\n");
+    }
+    printf("balance: %d\n", get_balance());
+  }
+
   return 0;
 }
