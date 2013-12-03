438d35d89a927ac69239071b3e542afe4db2978f
manemone <hamada.kazuki.mum@gmail.com>
2013-12-03 16:52:13 +0900

    課題6 のプログラムのインデントを調整。

--- practice_06/question_01/main.c
+++ practice_06/question_01/main.c
@@ -23,37 +23,37 @@ char *program_name = "sp6-server";
 int
 open_accepting_socket(int port)
 {
-        struct sockaddr_in self_addr;
-        socklen_t self_addr_size;
-        int sock, sockopt;
+  struct sockaddr_in self_addr;
+  socklen_t self_addr_size;
+  int sock, sockopt;
 
-        memset(&self_addr, 0, sizeof(self_addr));
-        self_addr.sin_family = AF_INET;
-        self_addr.sin_addr.s_addr = INADDR_ANY;
-        self_addr.sin_port = htons(port);
-        self_addr_size = sizeof(self_addr);
-        sock = socket(PF_INET, SOCK_STREAM, 0);
-        if (sock < 0)
-                logutil_fatal("accepting socket: %d", errno);
-        sockopt = 1;
-        if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR,
-            &sockopt, sizeof(sockopt)) == -1)
-                logutil_warning("SO_REUSEADDR: %d", errno);
-        if (bind(sock, (struct sockaddr *)&self_addr, self_addr_size) < 0)
-                logutil_fatal("bind accepting socket: %d", errno);
-        if (listen(sock, LISTEN_BACKLOG) < 0)
-                logutil_fatal("listen: %d", errno);
-        return (sock);
+  memset(&self_addr, 0, sizeof(self_addr));
+  self_addr.sin_family = AF_INET;
+  self_addr.sin_addr.s_addr = INADDR_ANY;
+  self_addr.sin_port = htons(port);
+  self_addr_size = sizeof(self_addr);
+  sock = socket(PF_INET, SOCK_STREAM, 0);
+  if (sock < 0)
+    logutil_fatal("accepting socket: %d", errno);
+  sockopt = 1;
+  if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR,
+        &sockopt, sizeof(sockopt)) == -1)
+    logutil_warning("SO_REUSEADDR: %d", errno);
+  if (bind(sock, (struct sockaddr *)&self_addr, self_addr_size) < 0)
+    logutil_fatal("bind accepting socket: %d", errno);
+  if (listen(sock, LISTEN_BACKLOG) < 0)
+    logutil_fatal("listen: %d", errno);
+  return (sock);
 }
 
 void
 usage(void)
 {
-        fprintf(stderr, "Usage: %s [option]\n", program_name);
-        fprintf(stderr, "option:\n");
-        fprintf(stderr, "\t-d\t\t\t\t... debug mode\n");
-        fprintf(stderr, "\t-p <port>\n");
-        exit(1);
+  fprintf(stderr, "Usage: %s [option]\n", program_name);
+  fprintf(stderr, "option:\n");
+  fprintf(stderr, "\t-d\t\t\t\t... debug mode\n");
+  fprintf(stderr, "\t-p <port>\n");
+  exit(1);
 }
 
 void
@@ -99,44 +99,44 @@ main_loop (int sock) {
 int
 main(int argc, char **argv)
 {
-        char *port_number = NULL;
-        int ch, sock, server_port = DEFAULT_SERVER_PORT;
-        int debug_mode = 0;
+  char *port_number = NULL;
+  int ch, sock, server_port = DEFAULT_SERVER_PORT;
+  int debug_mode = 0;
 
-        while ((ch = getopt(argc, argv, "dp:")) != -1) {
-                switch (ch) {
-                case 'd':
-                        debug_mode = 1;
-                        break;
-                case 'p':
-                        port_number = optarg;
-                        break;
-                case '?':
-                default:
-                        usage();
-                }
-        }
-        argc -= optind;
-        argv += optind;
+  while ((ch = getopt(argc, argv, "dp:")) != -1) {
+    switch (ch) {
+      case 'd':
+        debug_mode = 1;
+        break;
+      case 'p':
+        port_number = optarg;
+        break;
+      case '?':
+      default:
+        usage();
+    }
+  }
+  argc -= optind;
+  argv += optind;
 
-        if (port_number != NULL)
-                server_port = strtol(port_number, NULL, 0);
+  if (port_number != NULL)
+    server_port = strtol(port_number, NULL, 0);
 
-        /* server_portでlistenし，socket descriptorをsockに代入 */
-        sock = open_accepting_socket(server_port);
+  /* server_portでlistenし，socket descriptorをsockに代入 */
+  sock = open_accepting_socket(server_port);
 
-        if (!debug_mode) {
-                logutil_syslog_open(program_name, LOG_PID, LOG_LOCAL0);
-                daemon(0, 0);
-        }
+  if (!debug_mode) {
+    logutil_syslog_open(program_name, LOG_PID, LOG_LOCAL0);
+    daemon(0, 0);
+  }
 
-        /*
-         * 無限ループでsockをacceptし，acceptしたらそのクライアント用
-         * のスレッドを作成しプロトコル処理を続ける．
-         */
-        main_loop(sock);
+  /*
+   * 無限ループでsockをacceptし，acceptしたらそのクライアント用
+   * のスレッドを作成しプロトコル処理を続ける．
+   */
+  main_loop(sock);
 
-        /*NOTREACHED*/
-        return (0);
+  /*NOTREACHED*/
+  return (0);
 }
 
